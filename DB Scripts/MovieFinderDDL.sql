dd986d7ff3a57ef29a87bf3ca9c6363ee6d06bd2
SaranVad-1996 <saran.vadlamudi941@gmail.com>
2018-01-30 13:51:51 -0500

    Update MovieFinderDDL.sql

--- DB Scripts/MovieFinderDDL.sql
+++ DB Scripts/MovieFinderDDL.sql
@@ -1,27 +1,23 @@
-SET foreign_key_checks = 0;
-DROP TABLE IF EXISTS movie;
-DROP TABLE IF EXISTS genre;
-DROP TABLE IF EXISTS movie_genre;
 CREATE TABLE movie(
-						movieid INTEGER NOT NULL AUTO_INCREMENT,
-						title varchar(20),
-						description varchar(300),
+			movieid INTEGER NOT NULL AUTO_INCREMENT,
+			title varchar(20),
+			description varchar(300),
                         language varchar(10),
                         year INTEGER,
                         country varchar(15),
-						imagepath varchar(110),
-						PRIMARY KEY (movieid)
-				  );
+			imagepath varchar(110),
+			PRIMARY KEY (movieid)
+		  );
 
 CREATE TABLE genre(
-						genreid INTEGER NOT NULL AUTO_INCREMENT,
+			genreid INTEGER NOT NULL AUTO_INCREMENT,
                         genrename varchar(15),
-						PRIMARY KEY (genreid)
-				  );
+			PRIMARY KEY (genreid)
+		  );
                     
 CREATE TABLE movie_genre(
-							movieid INTEGER,
+			    movieid INTEGER,
                             genreid INTEGER,
                             FOREIGN KEY (movieid) REFERENCES movie(movieid),
                             FOREIGN KEY (genreid) REFERENCES genre(genreid)
-						);
+			);
